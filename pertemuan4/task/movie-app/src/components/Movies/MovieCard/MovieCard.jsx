3975db30cbe28af5c2b5a07780f56115bee414a4
notRaihan <justraihan@proton.me>
2024-04-25 11:41:49 +0700

    react list rendering, state, event listener
    - Refactor: Movies Component.
    - Rendering List.
    - Responding to Event.
    - State.

--- pertemuan4/task/movie-app/src/components/Movies/MovieCard/MovieCard.jsx
+++ pertemuan4/task/movie-app/src/components/Movies/MovieCard/MovieCard.jsx
@@ -1,17 +1,18 @@
 import styles from "./MovieCard.module.css";
 
-const MovieCard = () => {
-    return (
-        <div className={styles.movie}>
-            <img
-                className={styles.movie__image}
-                src="https://picsum.photos/300/400"
-                alt="placeholder"
-            />
-            <h3 className={styles.movie__title}>Movie Title</h3>
-            <p className={styles.movie__date}>Movie Date</p>
-        </div>
-    );
+const MovieCard = (props) => {
+  const { movie } = props;
+  return (
+    <div className={styles.movie}>
+      <img
+        className={styles.movie__image}
+        src={movie.poster}
+        alt={movie.title}
+      />
+      <h3 className={styles.movie__title}>{movie.title}</h3>
+      <p className={styles.movie__date}>{movie.year}</p>
+    </div>
+  );
 };
 
 export default MovieCard;
