86e9b45383c0a8d375f2fd48d7716dd3191411e1
raihandotmd <justraihan@proton.me>
2024-06-30 22:30:05 +0700

    fix: Home Movie not updated after add movie

--- pertemuan4/task/movie-app/src/pages/Home.jsx
+++ pertemuan4/task/movie-app/src/pages/Home.jsx
@@ -1,13 +1,10 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import Hero from "../components/Hero/Hero";
 import Movies from "../components/Movies/Movies";
 import MoviesContext from "../context/MoviesContext";
 import data from "../utils/constants/data";
 
 const Home = () => {
-  const { setMovies } = useContext(MoviesContext);
-
-  setMovies(data);
   return (
     <>
       <Hero />
